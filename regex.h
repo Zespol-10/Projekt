64731e27a4cde4e5a88dd3bf52590d79e048a64d
wojciechdomin <87430231+wojciechdomin@users.noreply.github.com>
2025-01-20 22:13:24 +0100

    Update regex.h
    Poprawki const char -----> char

--- regex.h
+++ regex.h
@@ -1,9 +1,9 @@
-int match(const char *text, const char * expr);
-int reg_ast(char c, const char *text, const char *expr);
-int reg_pl(char c, const char *text, const char *expr);
-int reg_br(char c, int m, int n, const char *pattern, const char *text);
+int match(char *text, char * expr);
+int reg_ast(char c,  char *text,  char *expr);
+int reg_pl(char c,  char *text,  char *expr);
+int reg_br(char c, int m, int n,  char *pattern,  char *text);
 int is_dgt(char c);
-int parse_dgt(const char **pattern);
+int parse_dgt( char **pattern);
 void reverse(char * s);
 void int_str(int n, char * s);
-int match_pos(const char *text, const char * expr);
+int match_pos( char *text,  char * expr);
