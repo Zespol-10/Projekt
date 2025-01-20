9232381c2b1828f57fa54d42363fd02747eac278
wojciechdomin <87430231+wojciechdomin@users.noreply.github.com>
2025-01-20 22:14:11 +0100

    Update regexpokaz.c
    dodanie include "regex.h"

--- regexpokaz.c
+++ regexpokaz.c
@@ -1,18 +1,83 @@
-#include <stdio.h>
-#include <string.h>
 #include "regex.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    char *expr[] = {strdup("a*b"), strdup("a+b"), strdup("a{2,4}b"), strdup("a.c"), strdup(".*ab$"), NULL};
-    char *text[] = {strdup("aab"), strdup("ab"), strdup("aaaab"), strdup("ababababb"), strdup("abababcabb"), NULL};
-    for(int i = 0; expr[i] != NULL; ++i){
-        printf("Testowanie: %s\n", expr[i]);
-        for(int j = 0; text[j] != NULL; ++j){
-            printf("    Text: %s -> ", text[j]);
-            if(match(expr[i], text[j])) printf("Znaleziono\n");
-            else printf("Nie znaleziono.\n");
+enum {FALSE, TRUE};
+
+int match(char *pattern, char *text);
+int reg_pos(char *pattern, char *text);
+int reg_ast(char c, char *pattern, char *text);
+int reg_pl(char c, char *pattern, char *text);
+int reg_br(char c, int m, int n, char *pattern, char *text);
+int is_digit(char c);
+int parse_dgt(char **pattern);
+
+int match(char *pattern, char *text){
+    do{
+        if(reg_pos(pattern, text)) return TRUE;
+    } while(*text++ != '\0');
+    return FALSE;
+}
+
+int reg_pos(char *pattern, char *text) {
+    if(pattern[0] == '\0') return TRUE;
+    if(pattern[1] == '*') return reg_ast(pattern[0], pattern + 2, text);
+    if(pattern[1] == '+') return reg_pl(pattern[0], pattern + 2, text);
+    if(pattern[1] == '{') {
+        char *p = pattern + 2;
+        int m = parse_dgt(&p);
+        int n = m;
+        if(*p == ','){
+            p++;
+            if(is_digit(*p)) n = parse_dgt(&p);
+            else n = -1;
         }
-        printf("\n");
+        if (*p == '}') return reg_br(pattern[0], m, n, p + 1, text);
     }
-    return 0;
+    if(pattern[0] == '$' && pattern[1] == '\0') return *text == '\0';
+    if(*text != '\0' && (pattern[0] == '.' || pattern[0] == *text)) return reg_pos(pattern + 1, text + 1);
+    return FALSE;
+}
+
+int reg_br(char c, int m, int n, char *pattern, char *text) {
+    int count = 0;
+    while (count < m) {
+        if (*text == '\0' || (*text != c && c != '.')) return FALSE;
+        text++;
+        count++;
+    }
+    while (n == -1 || count < n) {
+        if (reg_pos(pattern, text)) return TRUE;
+        if (*text == '\0' || (*text != c && c != '.')) break;
+        text++;
+        count++;
+    }
+    return reg_pos(pattern, text);
+}
+
+
+int reg_ast(char c, char *pattern, char *text) {
+    do{
+        if (reg_pos(pattern, text)) return TRUE;
+    } while(*text != '\0' && (*text++ == c || c == '.'));
+    return FALSE;
+}
+
+int reg_pl(char c, char *pattern, char *text) {
+    if(*text != '\0' && (*text == c || c == '.')) return reg_ast(c, pattern, text + 1);
+    return FALSE;
+}
+
+int is_digit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+int parse_dgt(char **pattern) {
+    int num = 0;
+    while (is_digit(**pattern)) {
+        num = num * 10 + (**pattern - '0');
+        (*pattern)++;
+    }
+    return num;
 }
